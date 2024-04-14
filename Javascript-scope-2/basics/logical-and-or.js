3677fb362f8bf6d55f6c3255b575e4f9207e1f8a
Geekazon <130860322+MatrixInnovations@users.noreply.github.com>
2024-04-13 20:58:37 -0400

    Update logical-and-or.js
    add my personal challenge area with my BIG O

--- Javascript-scope-2/basics/logical-and-or.js
+++ Javascript-scope-2/basics/logical-and-or.js
@@ -11,19 +11,35 @@ if (temp >= 60 && temp <= 90) {
     console.log('Eh. Do what you want')
 }
 
-// Challenge area
+// Challenge area 
 
-let isGuestOneVegan = false
-let isGuestTwoVegan = false
+let isGustOneVegan = true    // O(1)
+let isGustTwoVegan = false   // O(1)
 
-// Are both vegan? Only offer up vegan dishes.
-// At least one vegan? Make sure to offer up some vegan options.
-// Else, Offer up anything on the menue
+// Are both vegan? - Only offer up vegan dishes
+// At least one vegan? - Make sure to offer up some vegan options. 
+// Else, Offer up anything on the menu
 
-if (isGuestOneVegan && isGuestTwoVegan) {
-    console.log('Only offer up vegan dishes.')
-} else if (isGuestOneVegan || isGuestTwoVegan) {
-    console.log('Make sure to offer up some vegan options.')
-} else {
-    console.log('Offer up anything on the menu')
+
+
+if (isGustOneVegan === true && isGustTwoVegan === true) {    // O(1)
+	console.log('Only offer up vegan dishes')    // O(1)
+} else if (isGustOneVegan === true || isGustTwoVegan === true) {    // O(1)
+	console.log('Make sure to offer up some vegan options')    // O(1)
+} else {    // O(1)
+	console.log('Offer up anything on the menu')    // O(1)
 }
+
+
+The big O of the code above is O(1) this is constant time complexity. 
+
+In terms of time complexity, constant time complexity (O(1)) represents the fastest possible execution 
+time for an algorithm. It means that the algorithm's runtime does not grow with the size of the input.
+
+This is because the time taken to execute the algorithm remains constant, regardless of the size of the 
+input. In other words, the algorithm takes the same amount of time to complete its execution, regardless 
+of whether the input size is small or large.
+
+
+
+
